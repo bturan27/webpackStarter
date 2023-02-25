a4f7681f281a296469f0eb1faa029c962eb09faa
begum.turan <begum.turan@nomadgcs.com>
2023-02-24 17:03:21 -0700

    Initial commit

---
+++ webpack.config.js
@@ -0,0 +1,10 @@
+const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin')
+
+module.exports = {
+    entry: './src/index.js',
+    output: {
+        path: path.join(__dirname, '/dist'),
+        filename: "index_bundle.js"
+    }
+}
