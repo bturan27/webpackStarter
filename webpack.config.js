d7d9d163647d00cf4f8c5116be56946b4a7e43a3
begum.turan <begum.turan@nomadgcs.com>
2023-02-27 07:52:57 -0700

    webpack.config.js adjust

--- webpack.config.js
+++ webpack.config.js
@@ -1,10 +1,32 @@
 const path = require('path');
 const HtmlWebpackPlugin = require('html-webpack-plugin')
-
+//htmlwebpackplugin creates our index.html file for us
 module.exports = {
     entry: './src/index.js',
+    //index.js source icinde  entry file
     output: {
-        path: path.join(__dirname, '/dist'),
-        filename: "index_bundle.js"
-    }
+        //output where we want our compiled code to go it is gonna be bundle file. we are puttting the dist folder.we have to
+        //specicy the file name.
+        "path": path.join(__dirname, '/dist'),
+        "filename": "index_bundle.js"
+    },
+    module:{
+        rules: [
+            {
+                test: /\.js$/,
+                exclude: /node_modules/,
+                use:{
+                    loader:'babel-loader'
+                }
+            }
+        ]
+    },
+    plugins: [
+        new HtmlWebpackPlugin(
+            {
+                template:'./src/index.html'
+            }
+        )
+    ]
+
 }
